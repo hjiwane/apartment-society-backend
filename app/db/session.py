9afe29f0837905e76d53980e488d349fe328155f
hjiwane <himanshu.jiwane@gmail.com>
2026-02-11 15:56:56 -0500

    chore: add base and session

---
+++ app/db/session.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from ..core import config
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{config.settings.database_username}:{config.settings.database_password}@{config.setting.database_hostname}:{config.database_port}/{config.database_name}'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+ 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()   
+
+
+
